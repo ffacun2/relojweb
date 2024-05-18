d91e0a8d0acd6736c550f3bbafcc21c98e9efe42
Facundo Criado <facu-1999-44@hotmail.com>
2024-05-17 23:57:31 -0300

    Update cronometro.js

--- assets/script/cronometro.js
+++ assets/script/cronometro.js
@@ -1,4 +1,5 @@
 let [miliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let [flagHour, flagMinute, flagSecond, flagMilisecond] = [0,0,0,0];
 let control;
 let flagTime = 0;
 let contFlag = 0;
@@ -7,7 +8,7 @@ let tableBody = document.querySelector(".table-body");
 
 function start() {
 	inicial = Date.now();
-	control = setInterval(cronometro,1);
+	control = setInterval(cronometro,10);
 	document.getElementById("start-btn").disabled = true;
 	document.getElementById("pause-btn").disabled = false;
 	document.getElementById("restart-btn").disabled = true;
@@ -49,62 +50,42 @@ function reset() {
 
 function flag() {
 	contFlag++;
+	console.log(flagHour,flagMinute,flagSecond,flagMilisecond);
 	document.querySelector(".flag-conteiner").style.display = "block";
-	// flagTime = new Date();
-	
-	let [flagHour, flagMinute, flagSecond, flagMilisecond] = [
-		Math.floor((Date.now() - inicial) / (1000 * 60 * 60)),
-		Math.floor(((Date.now() - inicial) % (1000 * 60 * 60)) / (1000 * 60)),
-		Math.floor(((Date.now() - inicial) % (1000 * 60)) / 1000),
-		Date.now() - inicial,
-	];
-	console.log(new Date().setMilliseconds(Date.now-inicial));
-	inicial = Date.now();
-	let msj = "Mensaje";
+
 	let trFlag = document.createElement("tr");
 	trFlag.innerHTML = `
 					<th>${contFlag}</th>
-                    <th>${flagHour.toString().padStart(2, "0")}:${flagMinute
-		.toString()
-		.padStart(2, "0")}:${flagSecond
-		.toString()
-		.padStart(2, "0")} . ${flagMilisecond.toString().padStart(2, "0")}</th>
-                    <th>${hours.toString().padStart(2, "0")}:${minutes
-		.toString()
-		.padStart(2, "0")}:${seconds
-		.toString()
-		.padStart(2, "0")} . ${miliseconds.toString().padStart(2, "0")}</th>
+                    <th>${flagHour.toString().padStart(2, "0")}:${flagMinute.toString().padStart(2, "0")}:${flagSecond.toString().padStart(2, "0")} . ${flagMilisecond.toString().padStart(2, "0")}</th>
+                    <th>${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")} . ${miliseconds.toString().padStart(2, "0")}</th>
                 	`;
 	tableBody.insertBefore(trFlag, tableBody.children[0]);
 
+	[flagHour, flagMinute, flagSecond, flagMilisecond] = [0, 0, 0, 0];
 }
 
 function cronometro() {
 	miliseconds += 1;
+	flagMilisecond +=1;
 
 	if (miliseconds > 99) {
 		seconds++, (miliseconds = 0);
+		flagSecond++,(flagMilisecond = 0);
 	}
 	if (seconds > 59) {
 		minutes++, (seconds = 0);
+		flagMinute++,(flagSecond = 0);
 	}
 	if (minutes > 59) {
 		hours++, (minutes = 0);
+		flagHour++,(flagMinute = 0);
 	}
 	if (hours > 59) {
 		reset();
 	}
 
-	document.querySelector(".hh").innerHTML = `${hours
-		.toString()
-		.padStart(2, "0")}:`;
-	document.querySelector(".mm").innerHTML = `${minutes
-		.toString()
-		.padStart(2, "0")}:`;
-	document.querySelector(".ss").innerHTML = `${seconds
-		.toString()
-		.padStart(2, "0")}`;
-	document.querySelector(".ms").innerHTML = `.${miliseconds
-		.toString()
-		.padStart(2, "0")}`;
+	document.querySelector(".hh").innerHTML = `${hours.toString().padStart(2, "0")}:`;
+	document.querySelector(".mm").innerHTML = `${minutes.toString().padStart(2, "0")}:`;
+	document.querySelector(".ss").innerHTML = `${seconds.toString().padStart(2, "0")}`;
+	document.querySelector(".ms").innerHTML = `.${miliseconds.toString().padStart(2, "0")}`;
 }
